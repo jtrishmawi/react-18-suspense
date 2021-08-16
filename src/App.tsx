3dd2905a956879e547ec46390456fdb10d0510f7
Tarek Rishmawi <jtrishmawi@gmail.com>
2021-08-16 08:54:39 +0200

    rename admin page to spinner

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" exact>
             <Home />
           </Route>
-          <Route path="/admin" exact>
+          <Route path="/spinner" exact>
             <Spinner />
           </Route>
         </Switch>
